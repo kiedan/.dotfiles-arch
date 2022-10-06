ffedc46b0712b88d56cfd9705667072df0debb90
kiedan <net@kiedan.com>
2022-10-06 10:59:58 +0200

    polybar setup to work with i3 and openbox

--- polybar/.config/polybar/launch-openbox.sh
+++ polybar/.config/polybar/launch-openbox.sh
@@ -10,7 +10,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload mybar-i3 &
+    MONITOR=$m polybar --reload mybar-openbox &
 done
 fi
 
